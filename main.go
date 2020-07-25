f936544d9706b1f903b685c7a9f00fee2bd18703
ciiiii <allen.ccccnm@gmail.com>
2020-07-26 00:36:59 +0800

    add notification register static page

--- main.go
+++ main.go
@@ -12,6 +12,16 @@ func main() {
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 	gin.DisableConsoleColor()
+
+	r.LoadHTMLGlob("static/*.html")
+	r.Static("/css", "./static/css")
+	r.Static("/js", "./static/js")
+	r.StaticFile("/favicon.ico", "./static/favicon.ico")
+	r.StaticFile("/service-worker.js", "./static/service-worker.js")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
